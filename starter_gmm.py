de3ea43396531ba5b945585fd98927fa2ec892af
EricElmoznino <eric.elmoznino@gmail.com>
2019-03-16 17:37:47 -0400

    Did some reformatting

--- starter_gmm.py
+++ starter_gmm.py
@@ -3,32 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import helper as hlp
 
-# Loading data
-#data = np.load('data100D.npy')
-data = np.load('data2D.npy')
-[num_pts, dim] = np.shape(data)
-
-# For Validation set
-if is_valid:
-  valid_batch = int(num_pts / 3.0)
-  np.random.seed(45689)
-  rnd_idx = np.arange(num_pts)
-  np.random.shuffle(rnd_idx)
-  val_data = data[rnd_idx[:valid_batch]]
-  data = data[rnd_idx[valid_batch:]]
 
 # Distance function for GMM
-def distanceFunc(X, MU):
+def distance_func(x, mu):
     # Inputs
-    # X: is an NxD matrix (N observations and D dimensions)
-    # MU: is an KxD matrix (K means and D dimensions)
+    # x: is an NxD matrix (N observations and D dimensions)
+    # mu: is an KxD matrix (K means and D dimensions)
     # Outputs
     # pair_dist: is the pairwise distance matrix (NxK)
-    # TODO
+    # todo
+    pass
 
-def log_GaussPDF(X, mu, sigma):
+
+def log_gauss_pdf(x, mu, sigma):
     # Inputs
-    # X: N X D
+    # x: N X D
     # mu: K X D
     # sigma: K X 1
     # log_pi: K X 1
@@ -36,14 +25,17 @@ def log_GaussPDF(X, mu, sigma):
     # Outputs:
     # log Gaussian PDF N X K
 
-    # TODO
+    # todo
+    pass
 
-def log_posterior(log_PDF, log_pi):
+
+def log_posterior(log_pdf, log_pi):
     # Input
-    # log_PDF: log Gaussian PDF N X K
+    # log_pdf: log Gaussian PDF N X K
     # log_pi: K X 1
 
     # Outputs
     # log_post: N X K
 
-    # TODO
+    # todo
+    pass
