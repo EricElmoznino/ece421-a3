83c4dd8e518ea1ce267cd1bb8f21d8191ed95316
EricElmoznino <eric.elmoznino@gmail.com>
2019-03-21 14:51:07 -0400

    Removed unused packages in starter_kmeans.py

--- starter_kmeans.py
+++ starter_kmeans.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-import helper as hlp
 
 
-# Distance function for K-means
 def distance_func(x, mu):
     # Inputs
     # x: is an NxD matrix (N observations and D dimensions)
