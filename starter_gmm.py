8358da5bfceea9b5b52547499f0b44a8c5f5a72c
shawnsyl <shawnseyoung.lee@mail.utoronto.ca>
2019-03-27 22:54:25 -0400

    added loss and build_graph function

--- starter_gmm.py
+++ starter_gmm.py
@@ -7,6 +7,7 @@ import helper as hlp
 
 # Loading data
 #data = np.load('data100D.npy')
+"""
 data = np.load('data2D.npy')
 [num_pts, dim] = np.shape(data)
 
@@ -19,7 +20,7 @@ if is_valid:
   val_data = data[rnd_idx[:valid_batch]]
   data = data[rnd_idx[valid_batch:]]
 
-
+"""
 # Distance function for GMM
 def distance_func(x, mu):
     # Inputs
@@ -27,8 +28,10 @@ def distance_func(x, mu):
     # mu: is an KxD matrix (K means and D dimensions)
     # Outputs
     # pair_dist: is the pairwise distance matrix (NxK)
-    # todo
-    pass
+    x = tf.reshape(x, [-1, 1, int(x.shape[1])])
+    mu = tf.reshape(mu, [1, int(mu.shape[0]), int(mu.shape[1])])
+    pair_dist = tf.reduce_sum(((x - mu) ** 2), axis=-1)
+    return pair_dist
 
 
 def log_gauss_pdf(x, mu, sigma):
@@ -40,21 +43,37 @@ def log_gauss_pdf(x, mu, sigma):
 
     # Outputs:
     # log Gaussian PDF N X K
-
-    x = tf.reshape(x, [x.shape[0], 1, x.shape[1]])
-    mu = tf.reshape(mu, [1, mu.shape[0], mu.shape[1]])
-    log_gauss = -1 * tf.reduce_sum(((x - mu) ** 2), axis=-1) / (2 * sigma ** 2) - tf.math.log(
-        2 * tf.math.pi ** (mu.shape[1]/2) * sigma)
+    pair_dist = distance_func(x, mu)
+    log_gauss = -1 * pair_dist/ (2 * tf.reshape(sigma ** 2, [-1])) - int(mu.shape[1]) * tf.log(np.sqrt(2 * math.pi) * tf.reshape(sigma, [-1]))
     #log_gauss = -1 * tf.reduce_sum(((x - mu) ** 2), axis=-1) / (2 * sigma ** 2) - tf.math.log(
         #2 * tf.math.pi ** (mu.shape[1] / 2) * sigma ** 2) / 2
     return log_gauss
 
 
-def log_posterior(log_pdf):
+def log_posterior(log_pdf, log_pi):
     # Input
     # log_pdf: log Gaussian PDF N X K
     # log_pi: K X 1
 
     # Outputs
     # log_post: N X K
-    return log_pdf - hlp.logsoftmax(log_pdf)
+
+    return log_pdf + tf.reshape(log_pi, [-1]) - hlp.reduce_logsumexp(log_pdf + tf.reshape(log_pi, -[1]), reduction_indices=1, keep_dims = True) #iog_pdf- reduce_logsumexp(log_pdf, reduction_indices=0, keep_dims=True)
+
+def neg_log_prob(x, mu, sigma, log_pi):
+    log_pdf = log_gauss_pdf(x, mu, sigma) #pdf
+    log_likelihood = hlp.reduce_logsumexp(tf.exp(log_pdf+tf.reshape(log_pi, [-1])))
+    return -1 * log_likelihood
+
+def build_graph(d, k, lr):
+    x = tf.placeholder(dtype=tf.float32, shape=[None, d])
+    mu = tf.Variable(initial_value=tf.random_normal(shape=[k, d]))
+    sigma = tf.Variable(initial_value=tf.random_normal(shape=[k, 1]))
+    pi = tf.Variable(initial_value=tf.random_normal(shape=[k, 1]))
+    log_pi = hlp.logsoftmax(tf.reshape(pi,[-1]))
+    loss = neg_log_prob(x, mu, tf.exp(sigma), log_pi)
+    assignments = tf.argmin(loss, axis=-1)
+    loss = tf.reduce_sum(tf.reduce_min(loss, axis=-1))
+    optimizer = tf.train.AdamOptimizer(learning_rate=lr, beta1=0.9, beta2=0.99, epsilon=1e-5)
+    optimizer_op = optimizer.minimize(loss)
+    return x, mu, sigma, pi, assignments, loss, optimizer_op
